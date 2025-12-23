3ed463d82a74e543a7a7a97b14190636c3c9f003
Claire <claire.cheng@iisigroup.com>
2025-12-23 17:32:45 +0800

    chore: 新增 .gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,3 @@ export default defineConfig({
   // ⚠️ 重要：建立新專案後，請將 'repository-name' 改成你的 repository 名稱
   base: '/repository-name/',
 })
-```
-
----
-
-### 步驟 5：建立 .gitignore
-```
-# Logs
-logs
-*.log
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-pnpm-debug.log*
-lerna-debug.log*
-
-node_modules
-dist
-dist-ssr
-*.local
-
-# Editor directories and files
-.vscode/*
-!.vscode/extensions.json
-.idea
-.DS_Store
-*.suo
-*.ntvs*
-*.njsproj
-*.sln
-*.sw?
